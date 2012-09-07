fcb51aac501ccf0d11be90169632b882b30b8758
fetcher <fetcher@fetcher-MS-7366.(none)>
2012-09-07 18:22:44 -0300

    Vagrant and chef

---
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -0,0 +1,15 @@
+require_recipe "apt"
+require_recipe "apache2"
+
+require_recipe "rvm"
+require_recipe "mongodb"
+
+execute "disable-default-site" do
+  command "sudo a2dissite default"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
+
+web_app "project" do
+  template "project.conf.erb"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
